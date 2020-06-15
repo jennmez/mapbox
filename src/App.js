6dc870fe4bab34eae781f37a65436b242938faf3
jennmez <jennmez@gmail.com>
2020-06-15 12:18:08 -0500

    mapbox map created with Chicago ad center

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './mintbean.png';
 import './styles/index.css';
+import mapboxgl from 'mapbox-gl';
 
-function App() {
-  return (
-    <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-    </div>
-  );
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiamVubm1leiIsImEiOiJjazV3cGsxODYxeGtpM2ttcWZubDlzZG84In0.dId_CPazp07jO7R39YxC0w';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lng: -87.623177,
+      lat: 41.881832,
+      zoom: 10,
+    };
+  }
+  componentDidMount() {
+    const map = new mapboxgl.Map({
+      container: this.mapContainer,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: [this.state.lng, this.state.lat],
+      zoom: this.state.zoom,
+    });
+
+    map.on('move', () => {
+      this.setState({
+        // getCenter(): a Mapbox GL JS method, to get the new longitude and latitude of the point at the center of the map.
+        // toFixed(), a JavaScript method, to truncate the resulting floating point number to the specified number of digits
+        lng: map.getCenter().lng.toFixed(4),
+        lat: map.getCenter().lat.toFixed(4),
+        // getZoom(), a Mapbox GL JS method, to determine the zoom level that the map is set to.
+        zoom: map.getZoom.getZoom().toFixed(10),
+      });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <div ref={(el) => (this.mapContainer = el)} className="mapContainer" />
+      </div>
+    );
+  }
 }
 
 export default App;
